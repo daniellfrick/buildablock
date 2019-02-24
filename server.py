7c63708a5cb59eb6b85f4e919fade5f8c02f86d3
daniellfrick <8042056+daniellfrick@users.noreply.github.com>
2019-02-24 08:02:49 -0600

    d

--- server.py
+++ server.py
@@ -2,11 +2,27 @@ import locationdata
 import json
 
 from flask import Flask, request
+
+# Get a compiler
+from pybars import Compiler
+compiler = Compiler()
+
+# Compile the template
+x = open("template.html")
+source = x.read()
+template = compiler.compile(source)
+
 app = Flask(__name__)
 
+data_index = 0
+data = {}
+url="localhost:5000/"
 
 @app.route("/get/neighborhood", methods=['POST'])
 def neighborhood():
+    global data
+    global data_index
+    global url
     if request.method == "POST":
         print(request.data)
         gameState = request.get_json(force=True)
@@ -16,11 +32,25 @@ def neighborhood():
         maxScore = 0
         bestHood = None
         for s in scores:
-            if s[0] > maxScore:
-                maxScore = s[0]
+            print(s)
+            if s[0]['total'] > maxScore:
+                maxScore = s[0]['total']
                 bestHood = s[1]
+        scores.sort(reverse=True, key=lambda x:x[0]['total'])
         print(str(maxScore) + "  " + bestHood)
-        return "success"
+        data_index += 1
+        scores = list(map(lambda x: {'hood': x[1], 'score': x[0]['total'], 'subscores': x[0]['subscores']}, scores))
+        data[str(data_index)] = {'hoods': scores}
+        return url + str(data_index)
 
+@app.route('/<path:path>')
+def catch_all(path):
+    global data
+    global data_index
+    global template
+    print(data[path])
+    if path in data:
+        return template(data[path])
+    return "bad request"
 
 print("started")
